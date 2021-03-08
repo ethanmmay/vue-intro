3dcc4d11ead47647677b91261ed71e4a45b6b5e6
ethanmmay <ethaniel.may@gmail.com>
2021-03-08 12:03:57 -0700

    Basic Slap Game

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,35 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="root">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <div v-for="c in state.targets" :key="c.name">
+    <h2>{{ c.name }}</h2>
+    <h3 class="text-success">{{ c.health }}</h3>
+    <button class="btn btn-danger" @click="c.health -= 10">Slap</button>
+    <button class="btn btn-success" @click="c.health += 10">Heal</button>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import { reactive } from 'vue'
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  setup () {
+    const state = reactive({
+      targets: [
+        {
+          name: 'Master Evil Boss',
+          health: 100
+        },
+        {
+          name: 'Evil Grunt Dude',
+          health: 90
+        }
+      ]
+    })
+    return {
+      state
+    }
   }
 }
 </script>
